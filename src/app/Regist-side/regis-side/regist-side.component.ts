ea255f9665239125011a4ad7fabe810a8162bccf
Karol Szawara <karosza192@student.polsl.pl>
2022-11-13 20:33:26 +0100

    register form

--- src/app/Regist-side/regis-side/regist-side.component.ts
+++ src/app/Regist-side/regis-side/regist-side.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginServiceService } from 'src/app/login-service/login-service.service';
+import { RegisterProfile } from 'src/app/login-service/registerprofile';
+import { ConfirmedValidator } from './confirmed.validator';
 @Component({
   selector: 'app-regist-side',
   templateUrl: './regist-side.component.html',
@@ -8,24 +10,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistSideComponent implements OnInit {
 
-  registerForm=new FormGroup({
-    registerName:new FormControl('', [Validators.required]),
-    registerEmail: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}$")]),
-    registerPassword: new FormControl('', [Validators.required]),
-    registerAddress: new FormControl('', [Validators.required]),
-    registerNip:new FormControl('',[Validators.required, Validators.pattern("[0-9]{10}")]) ,    
-    registerCompanyName:new FormControl('', [Validators.required]),    
-    registerStreet:new FormControl('', [Validators.required]),
-    registerStreetNumber:new FormControl('', [Validators.required,Validators.pattern("[^[0-9]$]{10}")]) , 
-    registerFlatNumber:new FormControl('', [Validators.required,Validators.pattern("[^[0-9]$]{10}")])  ,
-    registerPostNumber:new FormControl('', [Validators.required,Validators.pattern("[0-9]{2}\-[0-9]{3}")]),        
-    registerPostTown: new FormControl('', [Validators.required]),
+  registerUser!:RegisterProfile;
+  public registerForm=new FormGroup({
+    
 
   })
 
-  constructor() { }
+  constructor(public fb: FormBuilder,private loginService:LoginServiceService) {
+    this.registerForm=fb.group({
+      registerFirstName:new FormControl('', [Validators.required,Validators.maxLength(50)]),
+      registerLastName:new FormControl('', [Validators.required,Validators.maxLength(50)]),
+      registerEmail: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}$")]),
+      registerPassword: new FormControl('', [Validators.required,Validators.maxLength(50),Validators.minLength(8)]),
+      registerConfirmPassword: new FormControl('', [Validators.required,Validators.maxLength(50),Validators.minLength(8)]),
+      registerAddress: new FormControl('', [Validators.required,Validators.maxLength(50)]),
+      registerNip:new FormControl('',[Validators.required, Validators.pattern("[0-9]{10}")]) ,    
+      registerCompanyName:new FormControl('', [Validators.required,Validators.maxLength(50)]),    
+      registerStreet:new FormControl('', [Validators.required,Validators.maxLength(50)]),
+      registerStreetNumber:new FormControl('', [Validators.required,Validators.pattern("^[0-9]*$"),Validators.maxLength(10)]) , 
+      registerFlatNumber:new FormControl('', [Validators.required,Validators.pattern("^[0-9]*$"),Validators.maxLength(10)])  ,
+      registerPostNumber:new FormControl('', [Validators.required,Validators.pattern("[0-9]{6}||[0-9]{2}\-[0-9]{3}")]),        
+      registerPostTown: new FormControl('', [Validators.required,Validators.maxLength(50)]),
+      registerPhoneNumber:new FormControl('', [Validators.required,Validators.maxLength(9)])
+    },{validator:ConfirmedValidator('registerPassword','registerConfirmPassword')})
+   }
 
   ngOnInit(): void {
   }
 
+ public submitRegister() {
+  console.log(this.registerForm.hasError);
+  console.log(this.registerForm.get("registerCompanyName")?.value);
+  console.log()
+  let tempUser:RegisterProfile=new RegisterProfile();
+  if(!this.registerForm.errors){
+    tempUser.companyName=this.registerForm.get("registerCompanyName")!.value;
+    tempUser.nip=this.registerForm.get("registerNip")!.value
+    tempUser.firstName=this.registerForm.get("registerFirstName")!.value
+    tempUser.lastName=this.registerForm.get("registerLastName")!.value
+    tempUser.street=this.registerForm.get("registerStreet")!.value
+    tempUser.streetNumber=this.registerForm.get("registerStreetNumber")!.value
+    tempUser.flatNumber=this.registerForm.get("registerFlatNumber")!.value
+    tempUser.postCode=this.registerForm.get("registerPostNumber")!.value
+    tempUser.postTown=this.registerForm.get("registerPostTown")!.value
+    tempUser.phonNumber=this.registerForm.get("registerPhoneNumber")!.value
+    tempUser.email=this.registerForm.get("registerEmail")!.value
+    tempUser.password=this.registerForm.get("registerPassword")!.value
+    if(tempUser.companyName!=null && tempUser.nip!=null){
+      tempUser.companyOrPerson='T';
+    }
+    else tempUser.companyOrPerson='F';
+    console.log(tempUser);
+    console.log(this.registerForm.get("registerPostNumber")!.value)
+    
+    this.loginService.registerUser(tempUser).subscribe(()=>{
+
+    })
+
+  }
+  
+
+}
 }
